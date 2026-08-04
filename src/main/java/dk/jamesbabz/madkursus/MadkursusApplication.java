63670c98ab24fd7edf211186b9543ae17f69ecda
JamesBabz <web.fuhlendorff@gmail.com>
2026-08-04 20:21:16 +0200

    initial commit

---
+++ src/main/java/dk/jamesbabz/madkursus/MadkursusApplication.java
@@ -0,0 +1,13 @@
+package dk.jamesbabz.madkursus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadkursusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadkursusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/jamesbabz/madkursus/MadkursusApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.jamesbabz.madkursus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadkursusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
